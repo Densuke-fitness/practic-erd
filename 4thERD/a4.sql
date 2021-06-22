7ce84dc3910fb904c54b563d135e4689386b4fc4
Densuke-fitness <densukefitness2020@gmail.com>
2021-06-22 15:48:03 +0900

    第4回を修正しました

--- 4thERD/a4.sql
+++ 4thERD/a4.sql
@@ -14,11 +14,11 @@ DROP TABLE IF EXISTS `ReceptionDetails`;
 
 CREATE TABLE `ReceptionDetails` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `reception_date` TIMESTAMP NULL DEFAULT NULL,
   `id_Reception` INTEGER NULL DEFAULT NULL,
   `id_MedicalClinics` INTEGER NULL DEFAULT NULL,
   `id_ContentOfConsultation` INTEGER NULL DEFAULT NULL,
   `id_Doctors` INTEGER NULL DEFAULT NULL,
-  `reception_date` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -31,9 +31,9 @@ DROP TABLE IF EXISTS `Reception`;
 
 CREATE TABLE `Reception` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_Patients` INTEGER NULL DEFAULT NULL,
   `date` DATETIME NULL DEFAULT 'NOT NULL',
-  `terminal_number` VARCHAR NULL DEFAULT NULL,
+  `id_Patients` INTEGER NULL DEFAULT NULL,
+  `id_Terminals` INTEGER NULL DEFAULT NULL,
   `account_number` INTEGER NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
@@ -62,6 +62,7 @@ DROP TABLE IF EXISTS `MedicalClinics`;
 
 CREATE TABLE `MedicalClinics` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -92,6 +93,47 @@ CREATE TABLE `Doctors` (
 );
 
 -- ---
+-- Table 'Terminals'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `Terminals`;
+
+CREATE TABLE `Terminals` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `number` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'MedicalDoctors'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `MedicalDoctors`;
+
+CREATE TABLE `MedicalDoctors` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id_MedicalClinics` INTEGER NULL DEFAULT NULL,
+  `id_Doctors` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'PossibleContentsOfConsultationByDepartment'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `PossibleContentsOfConsultationByDepartment`;
+
+CREATE TABLE `PossibleContentsOfConsultationByDepartment` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id_MedicalClinics` INTEGER NULL DEFAULT NULL,
+  `id_ContentOfConsultation` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
 -- Foreign Keys
 -- ---
 
@@ -100,6 +142,11 @@ ALTER TABLE `ReceptionDetails` ADD FOREIGN KEY (id_MedicalClinics) REFERENCES `M
 ALTER TABLE `ReceptionDetails` ADD FOREIGN KEY (id_ContentOfConsultation) REFERENCES `ContentOfConsultation` (`id`);
 ALTER TABLE `ReceptionDetails` ADD FOREIGN KEY (id_Doctors) REFERENCES `Doctors` (`id`);
 ALTER TABLE `Reception` ADD FOREIGN KEY (id_Patients) REFERENCES `Patients` (`id`);
+ALTER TABLE `Reception` ADD FOREIGN KEY (id_Terminals) REFERENCES `Terminals` (`id`);
+ALTER TABLE `MedicalDoctors` ADD FOREIGN KEY (id_MedicalClinics) REFERENCES `MedicalClinics` (`id`);
+ALTER TABLE `MedicalDoctors` ADD FOREIGN KEY (id_Doctors) REFERENCES `Doctors` (`id`);
+ALTER TABLE `PossibleContentsOfConsultationByDepartment` ADD FOREIGN KEY (id_MedicalClinics) REFERENCES `MedicalClinics` (`id`);
+ALTER TABLE `PossibleContentsOfConsultationByDepartment` ADD FOREIGN KEY (id_ContentOfConsultation) REFERENCES `ContentOfConsultation` (`id`);
 
 -- ---
 -- Table Properties
@@ -111,20 +158,29 @@ ALTER TABLE `Reception` ADD FOREIGN KEY (id_Patients) REFERENCES `Patients` (`id
 -- ALTER TABLE `MedicalClinics` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `ContentOfConsultation` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `Doctors` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Terminals` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `MedicalDoctors` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `PossibleContentsOfConsultationByDepartment` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `ReceptionDetails` (`id`,`id_Reception`,`id_MedicalClinics`,`id_ContentOfConsultation`,`id_Doctors`,`reception_date`) VALUES
+-- INSERT INTO `ReceptionDetails` (`id`,`reception_date`,`id_Reception`,`id_MedicalClinics`,`id_ContentOfConsultation`,`id_Doctors`) VALUES
 -- ('','','','','','');
--- INSERT INTO `Reception` (`id`,`id_Patients`,`date`,`terminal_number`,`account_number`) VALUES
+-- INSERT INTO `Reception` (`id`,`date`,`id_Patients`,`id_Terminals`,`account_number`) VALUES
 -- ('','','','','');
 -- INSERT INTO `Patients` (`id`,`name`,`ID`,`birth`) VALUES
 -- ('','','','');
--- INSERT INTO `MedicalClinics` (`id`) VALUES
--- ('');
+-- INSERT INTO `MedicalClinics` (`id`,`name`) VALUES
+-- ('','');
 -- INSERT INTO `ContentOfConsultation` (`id`,`name`) VALUES
 -- ('','');
 -- INSERT INTO `Doctors` (`id`,`name`) VALUES
 -- ('','');
+-- INSERT INTO `Terminals` (`id`,`number`) VALUES
+-- ('','');
+-- INSERT INTO `MedicalDoctors` (`id`,`id_MedicalClinics`,`id_Doctors`) VALUES
+-- ('','','');
+-- INSERT INTO `PossibleContentsOfConsultationByDepartment` (`id`,`id_MedicalClinics`,`id_ContentOfConsultation`) VALUES
+-- ('','','');
